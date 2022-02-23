169089eada1f247aa4e9f50a025b1b61a473e123
Rulian Dos Reis <rulian.dosreis@hotmail.com>
2022-02-23 02:42:24 -0300

    Feat: Form and continue button created

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import LogoImg from '../../assets/img/LogoCompassoBranco.png'
+import UserSvg from '../../assets/icons/VectorUser.svg'
+import PasswordSvg from '../../assets/icons/VectorPassword.svg'
 
 import { 
   LoginMainCointainer,
   LeftPanel,
   RightPanel,
   RightPanelLogo,
-  LoginInfoContainer
+  LoginInfoContainer,
+  LoginFormHeaderText,
+  LoginSubTextContainer,
+  LoginFormDescriptionText,
+  LoginFormInfoText,
+  LoginForm,
+  LoginInputContainer,
+  LoginInput, 
+  InputIcon,
+  LoginErrorMessageContainer,
+  ContinueButton
 } from './LoginScreen.style'
 
 export const LoginScreen = () => {
@@ -15,7 +27,25 @@ export const LoginScreen = () => {
       <LoginMainCointainer>
         <LeftPanel>
           <LoginInfoContainer>
-            
+            <LoginFormHeaderText>Olá,</LoginFormHeaderText>
+            <LoginSubTextContainer>
+              <LoginFormDescriptionText>Para continuar navegando de forma segura, efetue o login na rede.</LoginFormDescriptionText>
+            </LoginSubTextContainer>
+            <LoginFormInfoText>Login</LoginFormInfoText>
+            <LoginForm>
+              <LoginInputContainer>
+                <LoginInput type='text' placeholder='Usuário'/>
+                <InputIcon src={UserSvg}/>
+              </LoginInputContainer>
+              <LoginInputContainer>
+                <LoginInput type='password' placeholder='Senha'/>
+                <InputIcon src={PasswordSvg}/>
+              </LoginInputContainer>
+              <LoginErrorMessageContainer>
+                
+              </LoginErrorMessageContainer>
+            </LoginForm>
+            <ContinueButton>Continuar</ContinueButton>
           </LoginInfoContainer>
         </LeftPanel>
         <RightPanel>
