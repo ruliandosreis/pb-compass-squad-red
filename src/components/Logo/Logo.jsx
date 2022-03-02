b10e56dcfafad79107eabde48553ec71f569e5b1
Emerson Patryck <emeps@outlook.com>
2022-03-02 16:48:59 -0300

    feat: add logo white

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import LogoCompass from '../../assets/icons/LogoCompasso 1.svg'
-import { ImageContainer, ImageLogo } from './Logo.style';
+import LogoCompass from '../../assets/icons/LogoCompasso 1.svg';
+import LogoCompassWhite from '../../assets/img/LogoCompassoBranco.png';
+import { ImageContainer, LogoContainer } from './Logo.style';
 
 export const Logo = () => {
     return (
         <>
             <ImageContainer>
-                    {' '}
-                    <img
-                        src={LogoCompass}
-                        alt='Compass Logo'
-                    />
+                {' '}
+                <img src={LogoCompass} alt='Compass Logo' />
             </ImageContainer>
+        </>
+    );
+};
+
+export const LogoWhite = () => {
+    return (
+        <>
+            <LogoContainer>
+                {' '}
+                <img src={LogoCompassWhite} alt='Compass Logo' />;
+            </LogoContainer>
         </>
     );
 };
