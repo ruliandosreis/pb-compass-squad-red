5213a5d5fa8d3f314091169298fd9240e68f9ad6
Emerson Patryck <emeps@outlook.com>
2022-03-02 16:51:01 -0300

    fix: change unit of api - kelvin to celsius

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -13,7 +13,7 @@ import Cloud from '../../assets/icons/cloud.svg';
 const lat = '-28.26';
 const long = '-52.42';
 const apiKey = '4bee642f8c86df52bd2410efe3ad17b7';
-const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
+const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
 const UF = 43;
 const urlState = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${UF}`;
 
@@ -31,9 +31,7 @@ export const Weather = () => {
                     setWeather({
                         city: weather.data.name,
                         state: state.data.sigla,
-                        temperature: (weather.data.main.temp - 273.15).toFixed(
-                            0
-                        ),
+                        temperature: (weather.data.main.temp).toFixed(0),
                     });
                 })
             );
