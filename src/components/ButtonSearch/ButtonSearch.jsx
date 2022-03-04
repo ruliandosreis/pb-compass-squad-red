5f95e8c102a43a6da0b60a5af6bee9b34a473ca8
Rulian Dos Reis <rulian.dosreis@hotmail.com>
2022-03-04 17:05:57 -0300

    Feat: Added the Return to Page 2 function to the exit button

--- src/components/ButtonSearch/ButtonSearch.jsx
+++ src/components/ButtonSearch/ButtonSearch.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { ButtonsContainer, ButtonSearchStyle, ButtonExitStyle } from "./ButtonSearch.style";
 
 const buttonName = 'Buscar'
 const buttonExit = 'Sair'
 
+
 export const ButtonSearch = ()=>{
+
+    let returnPage2 = useNavigate();
     return(
         <ButtonsContainer>
             <ButtonSearchStyle>{buttonName}</ButtonSearchStyle>
-            <ButtonExitStyle>{buttonExit}</ButtonExitStyle>
+            <ButtonExitStyle onClick={()=>{
+              returnPage2('/');
+            }}>{buttonExit}</ButtonExitStyle>
         </ButtonsContainer>
     )
 }
